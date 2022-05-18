7330064fe664b2b8c7c9bc2e41c6314b21851bcc
Rijwol Shakya <shakyarijwol09@gmail.com>
2022-05-18 12:45:50 +0545

    next

--- lib/dao/student_dao.dart
+++ lib/dao/student_dao.dart
@@ -1,1 +1,11 @@
+import 'package:floor/floor.dart';
+import 'package:floor_database/entities/student.dart';
 
+@dao
+abstract class StudentDAO {
+  @insert
+  insertData(Student student);
+
+  @Query('select * from student')
+  List<Student> getAllStudents();
+}
--- lib/entities/student.dart
+++ lib/entities/student.dart
@@ -1,1 +1,13 @@
-class Student {}
+import 'package:floor/floor.dart';
+
+@entity
+class Student {
+  @PrimaryKey(autoGenerate: true)
+  int? id;
+  String fName;
+  String lName;
+  String username;
+  String password;
+
+  Student(this.fName, this.lName, this.username, this.password);
+}
